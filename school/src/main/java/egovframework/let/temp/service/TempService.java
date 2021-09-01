7e93880694e2f4cf7bb240a0ff26e2caa837c09d
Park Hyun Ah <Park Hyun Ah@192.168.0.33>
2021-09-01 17:33:23 +0900

    temp 만들어서.

---
+++ school/src/main/java/egovframework/let/temp/service/TempService.java
@@ -0,0 +1,8 @@
+package egovframework.let.temp.service;
+
+public interface TempService {
+
+	public TempVO selectTemp(TempVO vo) throws Exception;
+	
+	
+}
---
+++ school/src/main/java/egovframework/let/temp/service/TempVO.java
@@ -0,0 +1,24 @@
+package egovframework.let.temp.service;
+
+public class TempVO {
+	
+	private int tempId;
+	private String tempVal;
+	
+	
+	public int getTempId() {
+		return tempId;
+	}
+	public void setTempId(int tempId) {
+		this.tempId = tempId;
+	}
+	public String getTempVal() {
+		return tempVal;
+	}
+	public void setTempVal(String tempVal) {
+		this.tempVal = tempVal;
+	}
+	
+	
+
+}
